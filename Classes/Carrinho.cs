6b41956ad82ab52afe117a75631a9d9b68ea6227
LameckVieira <lameck.v.barbosa@gmail.com>
2020-12-17 09:43:36 -0300

    Objetos como argumentro

---
+++ Classes/Carrinho.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+
+namespace Classes
+{
+    public class Carrinho
+    {
+       public float ValorTotal { get; set; }
+
+       List<Produto> carrinho = new List<Produto>();
+
+       public void AdicionarProduto(Produto produto)
+       {
+           carrinho.Add(produto);
+       }
+
+       public void RemoverProduto(Produto produto)
+       {
+           carrinho.Remove(produto);
+       }
+
+       public void MostrarProduto()
+       {
+           if(carrinho != null)
+           {
+               foreach(Produto p in carrinho)
+               {
+                   Console.ForegroundColor = ConsoleColor.Green;
+                   Console.WriteLine($"R$ {p.Preco.ToString("n2")} - {p.Nome} ");
+                   Console.ResetColor();
+               }
+               MostrarTotal();
+
+           }
+       }
+       public void MostrarTotal()
+       {
+           Console.ForegroundColor = ConsoleColor.Blue;
+
+           if(carrinho != null)
+           {
+               foreach(Produto p in carrinho)
+               {
+                   ValorTotal += p.Preco;
+               }
+               Console.WriteLine($" Total do carrinho R$ {ValorTotal.ToString("n2")} ");    
+           }else{
+               Console.WriteLine($" Seu carrinho ainda está vazio! ");
+           }
+           Console.ResetColor();
+       }
+
+       public void AlterarItem(int _codigo, Produto _novoProduto){
+           carrinho.Find(x => x.Codigo == _codigo).Nome = _novoProduto.Nome;
+           carrinho.Find(x => x.Codigo == _codigo).Preco = _novoProduto.Preco;
+       }
+        
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Classes;
+
+namespace objetos_como_argumentos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p1 = new Produto(1, "Read dead Redemption 2", 200f);
+            Produto p2 = new Produto(2,"GTA 5", 80.99f);
+            Produto p3 = new Produto(3,"Detroit Become Huuman", 120.50f);
+            Produto p4 = new Produto(4,"Rainbow six siege", 22.99f);
+
+            Carrinho carrinho = new Carrinho();
+            carrinho.AdicionarProduto(p1);
+            carrinho.AdicionarProduto(p2);
+            carrinho.AdicionarProduto(p3);
+            carrinho.AdicionarProduto(p4);
+
+
+            carrinho.RemoverProduto(p2);// Remover produto
+
+            carrinho.MostrarProduto();
+        }
+    }
+}
